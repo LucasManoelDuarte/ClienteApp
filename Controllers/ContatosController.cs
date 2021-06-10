7d71eb255c45e75a66c12e941e1ab15a38243108
LucasManoelDuarte <84089272+LucasManoelDuarte@users.noreply.github.com>
2021-06-10 10:18:19 -0300

    Pasta contendo os Controladores

---
+++ Controllers/ContatosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClienteApp;
+using ClienteApp.Models;
+
+namespace ClienteApp.Controllers
+{
+    public class ContatosController : Controller
+    {
+        private readonly TabelaContext _context;
+
+        public ContatosController(TabelaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contatos
+        public async Task<IActionResult> Index()
+        {
+            var tabelaContext = _context.Contato.Include(c => c.Cliente);
+            return View(await tabelaContext.ToListAsync());
+        }
+
+        // GET: Contatos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contato = await _context.Contato
+                .Include(c => c.Cliente)
+                .FirstOrDefaultAsync(m => m.ClienteId == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            return View(contato);
+        }
+
+        // GET: Contatos/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Id");
+            return View();
+        }
+
+        // POST: Contatos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClienteId,Email,Telefone")] Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contato);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Id", contato.ClienteId);
+            return View(contato);
+        }
+
+        // GET: Contatos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contato = await _context.Contato.FindAsync(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Id", contato.ClienteId);
+            return View(contato);
+        }
+
+        // POST: Contatos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ClienteId,Email,Telefone")] Contato contato)
+        {
+            if (id != contato.ClienteId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contato);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContatoExists(contato.ClienteId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Id", contato.ClienteId);
+            return View(contato);
+        }
+
+        // GET: Contatos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contato = await _context.Contato
+                .Include(c => c.Cliente)
+                .FirstOrDefaultAsync(m => m.ClienteId == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            return View(contato);
+        }
+
+        // POST: Contatos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contato = await _context.Contato.FindAsync(id);
+            _context.Contato.Remove(contato);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContatoExists(int id)
+        {
+            return _context.Contato.Any(e => e.ClienteId == id);
+        }
+    }
+}
