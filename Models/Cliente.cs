8020529b3b30b63feddec7784467fb36d679be5a
LucasManoelDuarte <84089272+LucasManoelDuarte@users.noreply.github.com>
2021-06-10 10:26:35 -0300

    Arquivos contendo as classes (Models)

---
+++ Models/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClienteApp.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public bool ClienteAtivo { get; set; }
+        public string Nome { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataNascimento { get; set; }
+
+        public string Municipio { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataContrato { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataInclusao { get; set; }
+
+        public double ValorMensalContrato { get; set; }
+        public string Observacoes { get; set; }
+        public Contato ListaContato { get; set; }
+    }
+}
---
+++ Models/Contato.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClienteApp.Models
+{
+    public class Contato
+    {
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+
+    //    CREATE TABLE cliente(
+    //   codigo INTEGER PRIMARY KEY AUTOINCREMENT,
+    //   clienteAtivo BOOLEAN,
+    //   nome VARCHAR (200),
+    //   telefone VARCHAR(30),
+    //   municipio VARCHAR(150),
+    //   dataContrato DATE,
+    //   valorMensalContrato DECIMAL(17, 2),
+    //   observacoes TEXT,
+    //   dataInclusao DATETIME NOT NULL
+    //);
+
+    //CREATE TABLE contato(
+    //   id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //   codigoCliente INTEGER REFERENCES cliente (codigo),
+    //   nome VARCHAR (110),
+    //   dataNascimento DATE,
+    //   email VARCHAR(150),
+    //   telefone VARCHAR(30),
+    //   dataInclusao DATETIME NOT NULL
+    //);
+}
