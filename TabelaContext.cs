af28662562e37920517d1e87a411043d04fbc6ff
LucasManoelDuarte <84089272+LucasManoelDuarte@users.noreply.github.com>
2021-06-10 10:33:12 -0300

    Classe contexto

---
+++ TabelaContext.cs
@@ -0,0 +1,28 @@
+﻿using ClienteApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClienteApp
+{
+    public class TabelaContext : DbContext
+    {
+        public DbSet<Cliente> Cliente { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Contato>()
+                .HasKey("ClienteId");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TabelaDB;Trusted_Connection=true;");
+        }
+
+        public DbSet<ClienteApp.Models.Contato> Contato { get; set; }
+    }
+}
