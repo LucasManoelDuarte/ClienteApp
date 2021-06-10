24fee036d9fa54f654cebfcf0f57ad5b26306707
LucasManoelDuarte <84089272+LucasManoelDuarte@users.noreply.github.com>
2021-06-10 10:25:28 -0300

    Arquivos contendo as migrações

---
+++ Migrations/20210530232642_CriacaoTabela.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClienteApp.Migrations
+{
+    public partial class CriacaoTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClienteAtivo = table.Column<bool>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    DataNascimento = table.Column<DateTime>(nullable: false),
+                    Municipio = table.Column<string>(nullable: true),
+                    DataContrato = table.Column<DateTime>(nullable: false),
+                    DataInlusao = table.Column<DateTime>(nullable: false),
+                    ValorMensalContrato = table.Column<double>(nullable: false),
+                    Observacoes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contato",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contato", x => x.ClienteId);
+                    table.ForeignKey(
+                        name: "FK_Contato_Cliente_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Cliente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contato");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
---
+++ Migrations/20210531200929_ClienteAtualizacao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClienteApp.Migrations
+{
+    public partial class ClienteAtualizacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataInlusao",
+                table: "Cliente");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataInclusao",
+                table: "Cliente",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataInclusao",
+                table: "Cliente");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataInlusao",
+                table: "Cliente",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
